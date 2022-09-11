b97470327321562610a774c1e40bd0d5a62fce99
vyshnav <vyshnavnair378@gmail.com>
2022-09-11 13:15:59 +0530

    Add Environment variables

---
+++ app.js
@@ -0,0 +1,76 @@
+//jshint esversion:6
+require('dotenv').config()
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});   //connecting to the database using mongoose.
+
+const secret = "Thisisourlittlesecret."
+// Creating Schema through which new user data is stored. new mongoose.scheme is they syntax.
+const userSchema =new mongoose.Schema({
+  email: String,
+  password: String
+});
+//using mongoose-encrypt plugin to encrypt and decrypt secret key
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
+
+//creating model that can be used to create new users
+const User = new mongoose.model("User", userSchema);
+
+
+
+app.get("/", function(req, res){
+  res.render("home");
+});
+
+app.get("/login", function(req, res){
+  res.render("login");
+});
+
+app.get("/register", function(req, res){
+  res.render("register");
+});
+
+app.post("/register", function(req, res){
+  const newUser = new User({
+    email:req.body.username,
+    password: req.body.password
+  });
+newUser.save(function(err){
+  if(!err){
+    res.render("secrets");
+  } else{
+    console.log("error");
+  }
+});
+});
+
+app.post("/login", function(req, res){
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({email: username}, function(err, foundUser){
+    if(err){
+      console.log(err);
+    } else{
+    if(foundUser){
+      if(foundUser.password === password){
+        res.render("secrets");
+      }
+    }
+  }
+});
+});
+
+
+app.listen(3000, function(){
+   console.log("Server started at port: 3000");
+});
